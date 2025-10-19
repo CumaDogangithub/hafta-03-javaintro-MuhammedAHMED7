bf1ade8804df2f1293902ddd2d3123d56605f567
MuhammedAHMED7 <250541604@firat.edu.tr>
2025-10-20 00:00:10 +0300

    AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,13 @@
+250541604
+  Muhammed AHMED
+
+     class Main {
+    public static void main(String[] args) { 
+         System.out.println("   MM        MM  ");
+         System.out.println("   M M      M M  ");
+         System.out.println("   M  M    M  M  ");
+         System.out.println("   M   M  M   M  ");
+         System.out.println("   M    MM    M  ");
+   }
+   
+}
