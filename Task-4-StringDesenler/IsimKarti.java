8c421a6c755f59eade5352de90df15648b1c4896
MuhammedAHMED7 <250541604@firat.edu.tr>
2025-10-19 22:03:17 +0300

    simKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,20 @@
+250541604
+  Muhammed AHMED
+
+     public class İsimKarti {
+    public static void main(String[] args) {
+    String ad= "Muhammed";
+    String soyad="AHMED";
+    String ogrno="250541604";
+    String bolum="yazilim Muh.";
+
+    System.out.println(",,,,,,,,,,,,,");
+    System.out.println("Ogrenci bilgileri");
+    System.out.println(",,,,,,,,,,,,,,,,,,,,");
+    System.out.println("Ad :" + ad);
+    System.out.println("Soyad  :"  +soyad);
+    System.out.println("No :" +ogrno);
+    System.out.println("BOlum :"+bolum);
+     System.out.println(",,,,,,,,,,");
+    }
+}
